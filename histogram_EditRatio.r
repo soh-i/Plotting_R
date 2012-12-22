3d705f1e5a738c97784bf2214304e30300983809
soh-i <soh.ishiguro3914@gmail.com>
2012-12-23 00:14:15 +0900

    Fixed indent

--- histogram_EditRatio.r
+++ histogram_EditRatio.r
@@ -8,16 +8,16 @@ g <- ggplot (
   edit_df,
   aes ( x = value )
   ) +
-    labs( 
-      title = "Histogram of A-to-G editing level",
-      y = "Frequency",
-      x = "Editing ratio"
+  labs( 
+    title = "Histogram of A-to-G editing level",
+    y = "Frequency",
+    x = "Editing ratio"
     ) +
-    geom_histogram (
-      binwidth = 0.01,
-      alpha = 0.8,
-      fill = "red",
+  geom_histogram (
+    binwidth = 0.01,
+    alpha = 0.8,
+    fill = "red",
     )
-        
+
 plot(g)
 ggsave( filename=(paste("Editing_ratio.png")), plot=g, width=6, height=4, dpi = 300 )
