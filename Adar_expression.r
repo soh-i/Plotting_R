3f9e6d6eeccb418fac8bc0e2709d2665adb68b25
soh-i <soh.ishiguro3914@gmail.com>
2012-12-23 00:20:46 +0900

    Fixed indent

--- Adar_expression.r
+++ Adar_expression.r
@@ -1,26 +1,28 @@
 library(ggplot2)
 library(reshape)
 
-adar_fpkm <- read.table("./result_data/Adar_yok.fpkm", header=T)
-adar_df <- melt(adar_fpkm)
+adar_fpkm <- read.table( "./result_data/Adar_yok.fpkm", header=T )
+adar_df   <- melt( adar_fpkm )
 
 g <- ggplot(
   adar_df,
-  aes( x = variable,
-       y = value,
-       fill=Adar1
+  aes( x    = variable,
+       y    = value,
+       fill = Adar1
        )
   ) +
-    geom_bar( width = 0.5,
-              alpha = 0.7
-              ) +
-    #scale_fill_manual(values=c("gray") ) +
-    ylim(0,80) + 
-    labs( title = "Expression level of ADAR homolog",
-          y = "FPKM",
-          x = ""
-    ) + theme(legend.position = "none") # legend is invisible
+  geom_bar(
+    width = 0.5,
+    alpha = 0.7
+    ) +
+  ylim( 0,80 ) + 
+  labs(
+    title = "Expression level of ADAR homolog",
+    y     = "FPKM",
+    x     = ""
+    ) +
+  theme( legend.position = "none" ) # legend is invisible
 
-ggsave( filename=(paste("Adar_expression_yok.png")), plot=g, width=6, height=4, dpi = 300, path="./figure/")
+ggsave( filename=(paste("Adar_expression_yok.png")), plot=g, width=6, height=4, dpi = 300, path="./figure/" )
 
       
