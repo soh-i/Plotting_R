e7ed2cf59958259a1ffa15c7bc5d37c83c8e922c
soh-i <soh.ishiguro3914@gmail.com>
2012-12-23 00:13:36 +0900

    Fixed indent

--- pred_acc.r
+++ pred_acc.r
@@ -6,14 +6,18 @@ df <- melt(data)
 
 g <- ggplot(
   df,
-  aes( y = data$V2[1],
-       x = data$V2[2]
-  )
-  ) + geom_point(colour = "orange",
-                 size = 6
-                 ) + ylim(0,1) + xlim(0,1) + 
-                   labs (title="Comparison of previous identified editing site in dmel",
-                         y = "Recall",
-                         x = "Precision")
+  aes(
+    y = data$V2[1],
+    x = data$V2[2] )
+  ) + geom_point(
+    colour = "orange",
+    size = 6
+    ) + ylim(0,1) + xlim(0,1) + 
+  labs(
+    title="Comparison of previous identified editing site in dmel",
+    y = "Recall",
+    x = "Precision"
+    )
+
 plot(g)
 ggsave( filename=(paste("PrecisioRecall_plot.png")), plot=g, width=6, height=4, dpi = 300 )
