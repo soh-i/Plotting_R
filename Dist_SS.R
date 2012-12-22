ab84c0aee2473bc1bfe8274cd41a5e34aa45e956
soh-i <soh.ishiguro3914@gmail.com>
2012-12-23 00:18:50 +0900

    Fixed indent

--- Dist_SS.R
+++ Dist_SS.R
@@ -2,22 +2,22 @@ library(ggplot2)
 library(reshape)
 
 DistfromSpliceSite <- read.table("/Volumes/BI/RNA_editing/Dist_spliceSite_all.list", header=F)
-distSS_df <- melt(DistfromSpliceSite$V1)
+distSS_df          <- melt(DistfromSpliceSite$V1)
 
 g <- ggplot (
   distSS_df,
-  aes ( x = value )
+  aes( x = value )
 ) +
   labs( 
     title = "Distance from splice sites of putative editing sites",
-    y = "Frequency",
-    x = "Relative position(bp)"
-  ) +
-    geom_histogram (
-      binwidth = 10,
-      alpha = 0.8,
-      fill = "blue" 
-      ) + xlim (-1000, 1000)
+    y     = "Frequency",
+    x     = "Relative position(bp)"
+    ) +
+  geom_histogram(
+    binwidth = 10,
+    alpha    = 0.8,
+    fill     = "blue" 
+    ) + xlim( -1000, 1000 )
 
 plot(g)
 ggsave( filename=(paste("Distance_SS.png")), plot=g, width=6, height=4, dpi = 300 )
