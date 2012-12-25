6f87fc71a437d1c65467376e45d35403bb9d50ed
soh-i <soh.ishiguro3914@gmail.com>
2012-12-25 15:08:33 +0900

    Fixed indent

--- barplot_editing_freq.r
+++ barplot_editing_freq.r
@@ -2,42 +2,41 @@ library(ggplot2)
 library(reshape)
 
 ### Plotting overview of all base substitution type for each samples
-Initial_set = read.table("./Initial_set.txt", header=T)
-Final_set   = read.table("./FinalFiteredSet.txt", header=T)
+Initial_set <- read.table("./Initial_set.txt", header = TRUE)
+Final_set   <- read.table("./FinalFiteredSet.txt", header = TRUE)
 
 # Plottting initial set
 init_df <- melt(Initial_set)
-g_init <- ggplot (
+g_init  <- ggplot(
   init_df,
-  aes (
-    x = variable, 
-    y = value,      
+  aes(
+    x    = variable, 
+    y    = value,      
     fill = Type
     )
   ) + geom_bar() +
-  scale_fill_brewer("RNA-seq condition", palette="Oranges") +
+  scale_fill_brewer("RNA-seq condition", palette = "Oranges") +
   labs (
     title = "All possible editing types in the initial candidate set",
-    y = "Mismatch count",
-    x = ""
+    y     = "Mismatch count",
+    x     = ""
     )
-ggsave( filename=(paste("Initial_call.png")), plot=g_init, width=6.1, height=6, dpi = 300 )
+ggsave(filename = (paste("Initial_call.png")), plot = g_init, width = 6.1, height = 6, dpi = 300)
 
 # Plotting filtered set
 fin_df <- melt(Final_set)
-g_filt <- ggplot (
+g_filt <- ggplot(
   fin_df,
-  aes (
-    x = variable,
-    y = value,      
+  aes(
+    x    = variable,
+    y    = value,      
     fill = Type
     )
   ) + geom_bar() +
-  scale_fill_brewer("RNA-seq condition", palette="Oranges") +
-  labs (
+  scale_fill_brewer("RNA-seq condition", palette = "Oranges") +
+  labs(
     title = "Filnal filtered set",
-    y = "Mismatch count",
-    x = ""
+    y     = "Mismatch count",
+    x     = ""
     )
-ggsave( filename=(paste("Final_Filted_call.png")), plot=g_filt, width=6.1, height=6, dpi = 300 )
-
+ggsave(filename = (paste("Final_Filted_call.png")), plot = g_filt, width = 6.1, height = 6, dpi = 300)
