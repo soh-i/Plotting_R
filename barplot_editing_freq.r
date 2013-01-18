77cef48fdf58e1378a37166e9b79613251552d31
soh-i <soh.ishiguro3914@gmail.com>
2013-01-18 20:32:07 +0900

    ラベルとグラフの色を対応させた。

--- barplot_editing_freq.r
+++ barplot_editing_freq.r
@@ -2,29 +2,35 @@ library(ggplot2)
 library(reshape)
 
 ### Plotting overview of all base substitution type for each samples
-Initial_set <- read.table("./Initial_set.txt", header = TRUE)
-Final_set   <- read.table("./FinalFiteredSet.txt", header = TRUE)
+Initial_set <- read.table("./result_data/Initial_set.txt", header = TRUE)
+Final_set   <- read.table("./result_data/FinalFiteredSet.txt", header = TRUE)
 
 # Plottting initial set
 init_df <- melt(Initial_set)
+init_df$Type <- factor(init_df$Type, c("Active", "Tun", "Recovery_80min", "Recovery_3h"))
+
 g_init  <- ggplot(
   init_df,
   aes(
     x    = variable, 
-    y    = value,      
+    y    = value,
     fill = Type
     )
   ) + geom_bar() +
-  scale_fill_brewer("RNA-seq condition", palette = "Oranges") +
-  labs (
-    title = "All possible editing types in the initial candidate set",
-    y     = "Mismatch count",
-    x     = ""
-    )
-ggsave(filename = (paste("Initial_call.png")), plot = g_init, width = 6.1, height = 6, dpi = 300)
+    scale_fill_brewer( name = "RNA-seq condition",
+                         breaks=c( "Recovery_3h", "Recovery_80min", "Tun", "Active" )) +
+      labs (  
+        #title = "All possible editing types in the initial candidate set",
+        y     = "Mismatch count",
+        x     = ""
+        )
+plot(g_init)
+ggsave(filename = (paste("Initial_call.png")), plot = g_init, width = 8, height = 4, dpi = 300)
 
 # Plotting filtered set
 fin_df <- melt(Final_set)
+fin_df$Type <- factor(fin_df$Type, c("Active", "Tun", "Recovery_80min", "Recovery_3h"))
+
 g_filt <- ggplot(
   fin_df,
   aes(
@@ -33,10 +39,13 @@ g_filt <- ggplot(
     fill = Type
     )
   ) + geom_bar() +
-  scale_fill_brewer("RNA-seq condition", palette = "Oranges") +
+  scale_fill_brewer("RNA-seq condition",
+                    breaks=c( "Recovery_3h", "Recovery_80min", "Tun", "Active" )) +
   labs(
-    title = "Filnal filtered set",
+    #title = "Filnal filtered set",
     y     = "Mismatch count",
     x     = ""
     )
-ggsave(filename = (paste("Final_Filted_call.png")), plot = g_filt, width = 6.1, height = 6, dpi = 300)
+
+ggsave(filename = (paste("Final_Filted_call.png")), plot = g_filt, width = 8, height = 4, dpi = 300)
+
