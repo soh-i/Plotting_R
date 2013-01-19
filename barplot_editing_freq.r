1d3f54f12f6b26b5583a2ba235b78c6f2007ee81
soh-i <soh.ishiguro3914@gmail.com>
2013-01-19 17:34:51 +0900

    インデントの直し、epsで出力

--- barplot_editing_freq.r
+++ barplot_editing_freq.r
@@ -2,14 +2,14 @@ library(ggplot2)
 library(reshape)
 
 ### Plotting overview of all base substitution type for each samples
-Initial_set <- read.table("./result_data/Initial_set.txt", header = TRUE)
-Final_set   <- read.table("./result_data/FinalFiteredSet.txt", header = TRUE)
+Initial_set <- read.table( "./result_data/Initial_set.txt", header=TRUE )
+Final_set   <- read.table( "./result_data/FinalFiteredSet.txt", header=TRUE )
 
 # Plottting initial set
-init_df <- melt(Initial_set)
-init_df$Type <- factor(init_df$Type, c("Active", "Tun", "Recovery_80min", "Recovery_3h"))
+init_df      <- melt( Initial_set )
+init_df$Type <- factor( init_df$Type, c("Active", "Tun", "Recovery_80min", "Recovery_3h") )
 
-g_init  <- ggplot(
+g_init <- ggplot(
   init_df,
   aes(
     x    = variable, 
@@ -17,19 +17,20 @@ g_init  <- ggplot(
     fill = Type
     )
   ) + geom_bar() +
-    scale_fill_brewer( name = "RNA-seq condition",
-                         breaks=c( "Recovery_3h", "Recovery_80min", "Tun", "Active" )) +
-      labs (  
-        #title = "All possible editing types in the initial candidate set",
-        y     = "Mismatch count",
-        x     = ""
-        )
-plot(g_init)
-ggsave(filename = (paste("Initial_call.png")), plot = g_init, width = 8, height = 4, dpi = 300)
+  scale_fill_brewer(
+    name   = "RNA-seq condition",
+    breaks = c( "Recovery_3h", "Recovery_80min", "Tun", "Active" )) +
+  labs(
+    #title = "",
+    y     = "Mismatch count",
+    x     = ""
+    )
+#plot( g_init )
+ggsave( filename=(paste( "Initial_call.eps" )), plot=g_init, width=8, height=4, dpi=300, path="./figure/" )
 
 # Plotting filtered set
-fin_df <- melt(Final_set)
-fin_df$Type <- factor(fin_df$Type, c("Active", "Tun", "Recovery_80min", "Recovery_3h"))
+fin_df      <- melt( Final_set )
+fin_df$Type <- factor( fin_df$Type, c( "Active", "Tun", "Recovery_80min", "Recovery_3h" ))
 
 g_filt <- ggplot(
   fin_df,
@@ -39,13 +40,15 @@ g_filt <- ggplot(
     fill = Type
     )
   ) + geom_bar() +
-  scale_fill_brewer("RNA-seq condition",
-                    breaks=c( "Recovery_3h", "Recovery_80min", "Tun", "Active" )) +
+  scale_fill_brewer(
+    name   = "RNA-seq condition",
+    breaks = c( "Recovery_3h", "Recovery_80min", "Tun", "Active" )) +
   labs(
     #title = "Filnal filtered set",
     y     = "Mismatch count",
     x     = ""
-    )
+  )
 
-ggsave(filename = (paste("Final_Filted_call.png")), plot = g_filt, width = 8, height = 4, dpi = 300)
+ggsave( filename=(paste( "Final_Filted_call.eps" )), plot=g_filt, width=8, height=4, dpi=300, path="./figure/" )
+
 
